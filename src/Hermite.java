01a22f5960eb0d095f33a07a8d70a83014c57917
Maciej Kucharski <maciej.kucharski@ydp.eu>
2013-11-06 03:02:10 +0100

    To tak:
    wprowadzilem kilka zmian
    1) inny format wyswietlanai danych (u siebie mialem rozpisany algorym w takiej postaci w jakiej teraz sie wyswietla, nie chcialo mi sie go przerabiac wiec zmieniłem kod)
    2) poki co dane sa na sztywno -- z tymi danymi dzialaja, jest 03 w nocy wiec juz nie bede sprawdzal na innych -- nadzieja matka glupich ze dziala dla innych. ALe dla testowych dziala super.
    3) teraz z tego trzeba wyciagnac wielomian, ale to juz powinno byc znosne, Jutro moze to zrobie.
    4) tablice zmienilem na float, zeby ulamki sie wyswietlaly
    5) Algorytm nie jest piekny i jest bardzo wrazliwy, ale na dzisiejsza noc to jest i tak dobrze bo myslalem ze wlg tego nie zerobie dzisiaj, takze podstawa jakas jest do pracy
    6) Z rewiev Twojego kodu: to poprawilem polskie znami i zamknalem strumien
    
    Dla potwierdzenia na trello (https://trello.com/b/IOuB9k6w/metodyobliczeniowe) w zalaczniku taska wrzucilem zdjecie kartki mojej z wyliczeniami. Zgadzaja sie, a co najciekawsze recznie kontrolowalem do 2 przejscia funcji, potem nagel samo wyskoczylo poprawnie, wiec moge sadzic ze jest ok. Bo nie musialem sledzic tego do konca za raczke, tylko algorytm potem sam zaczal lapac o co kaman.
    
    Uwagi, bledy itp: na trello w komentach

--- src/Hermite.java
+++ src/Hermite.java
@@ -2,74 +2,136 @@ import java.util.Scanner;
 
 public class Hermite {
 
-	private final static int N = 4;
-
-	public static void pobierzDane(int[] x, int[] y, int[] yP, int n) {
-
+	private static int iloscElementow = 5;
+	public static void pobierzDane(float[] x, float[] y, float[] yP) {
+		
 		Scanner in = new Scanner(System.in);
-		System.out.println("Witaj u¿ytkowniku!");
-
+		System.out.println("Witaj uzytkowniku!");
+		System.out.println("Podaja ilosc elementow");
+		iloscElementow = in.nextInt();
+		
 		// Wypelnianie tablicy x
-		for (int i = 0; i < n; i++) {
+		for (int i = 0; i < iloscElementow; i++) {
 
-			System.out.println("Podaj wartoœæ x dla x" + i + ":");
+			System.out.println("Podaj wartosc x dla x" + i + ":");
 			x[i] = in.nextInt();
 
 		}
 
 		// Wypelnianie tablicy y
-		for (int i = 0; i < n; i++) {
+		for (int i = 0; i < iloscElementow; i++) {
 
-			System.out.println("Podaj wartoœci y dla x" + i + ":");
+			System.out.println("Podaj wartosci y dla x" + i + ":");
 			y[i] = in.nextInt();
 
 		}
 
 		// Wypelnianie tablicy yP
 
-		int iloscyP = (n / 2) + 1;
+		int parzystaIloscElementow  = (iloscElementow / 2) + 1;
 
-		for (int i = 0; i < iloscyP; i++) {
+		for (int i = 0; i < parzystaIloscElementow; i++) {
 
-			System.out.println("Podaj wartoœci yP dla x" + 2 * i + ":");
+			System.out.println("Podaj wartosci yP dla x" + 2 * i + ":");
 
 			yP[2 * i] = in.nextInt();
 		}
-
+		in.close();
 	}
 
-	public static void pokazTabelke(int[] x, int[] y, int[] yP, int n) {
+	public static void pokazTabelke(float[] x, float[] y, float[] yP) {
 		
 		System.out.println("Tabelka wartosci:");
-		System.out.print("x:    ");
-		for (int i = 0; i < n; i++) {
+		System.out.print("x:     ");
+		for (int i = 0; i < iloscElementow; i++) {
 
-			System.out.print(" |"+ x[i]);
+			System.out.print("  |"+ x[i]);
+			
+			
+			
+		
 		}
 		System.out.println();
-		System.out.print("f(x): ");
-		for (int i = 0; i < n; i++) {
-
-			System.out.print(" |" + y[i]);
+		System.out.print("f(x):  ");
+		for (int i = 0; i < iloscElementow; i++) {
+			System.out.print("  |" + y[i]);
+			
 
 		}
-
 		System.out.println();
-		System.out.print("f'(x):");
-		for (int i = 0; i < n; i++) {
-
-			System.out.print(" |" + yP[i] );
+		System.out.print("f'(x): ");
+		for (int i = 0; i < iloscElementow; i++) {
+			System.out.print("  |" + yP[i] );
+			
 		}
 	}
+	
+	public static void pokazKolumnyDoAlgorytmu(float[] x, float[] y){
+		System.out.println();
+		System.out.println("kolumna do algorytmu dla ulatwienia widoku");
+		System.out.println("x   |    y");
+		System.out.println("----------");
+		for (int i = 0; i < iloscElementow; i++) {
+			System.out.print("" + x[i]);
+			System.out.print("   |    " + y[i] );
+			System.out.println();
+		}
+		
+	}
 
+	
+	public static void wylicz(float[] x, float[] y, float[] yP) {
+		int licznikPrzejsc = 0;
+		int licznikDlaPrim = 0;
+		float wynik  = 0;
+		while(licznikPrzejsc != 4) {
+			
+			
+			for (int i = 0; i< iloscElementow-1; i++){
+				
+				float licznik = y[i+1]-y[i];
+				float mianownik = x[i+1+licznikPrzejsc]-x[i];
+				
+				if (mianownik == 0) {
+					
+					wynik = yP[licznikDlaPrim];
+					y[i] = wynik;
+					licznikDlaPrim++;
+				}
+				else {
+					
+					wynik = licznik / mianownik;
+					y[i]=wynik;
+				}
+//				System.out.println("----");
+//				System.out.println(wynik + "    i    " + i);
+				
+			}
+			
+			System.out.println();
+			for (int i = 0; i< iloscElementow-1; i++)
+				System.out.print(y[i] + " ");
+			System.out.println();
+			//pokazKolumnyDoAlgorytmu(x, y);
+			licznikPrzejsc++;
+			iloscElementow--;
+			
+		}
+	}
+	
+
+	
 	public static void  main(String[] arg) {
 		
-		int[] x = new int[10];
-		int[] y = new int[10];
-		int[] yP = new int[10];
 
-		pobierzDane(x, y, yP, N);
-		pokazTabelke(x,y,yP,N);
+		
+		float[] x = {0,0,1,2,2};
+		float[] y = {0,0,1,2,2};
+		float[] yP = {1,-1,1};
+		
+		pokazKolumnyDoAlgorytmu(x, y);
+	
+		wylicz(x,y, yP );
 	}
 
 }
