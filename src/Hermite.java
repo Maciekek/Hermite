b1beafd831627efb59d594c2df365bb2d7c3a28e
Maciej Kucharski <maciej.kucharski@ydp.eu>
2013-11-11 00:07:03 +0100

    Troche poluzowane ograniczenia: zwiekszone tablice i ponowne podpiecie wczytywania danych od uzytkownika

--- src/Hermite.java
+++ src/Hermite.java
@@ -5,41 +5,48 @@ public class Hermite {
 	private static int iloscElementow;
 	private static int iloscElementowX;
 	private static int parzystaIloscElementow;
-	private static float[] rowX = new float[10];
-	private static float[] rowY = new float[10];
-	static float[] x = new float[7];
-	static float[] y = new float[7];
-	static float[] yP = new float[5];
-	static float[] wynik = new float[6];
-	static float[] wynikPoSumowaniu = new float[6];
-	private static float[] wspolczynnikWielomianu = new float[5];
+	private static float[] rowX = new float[20];
+	private static float[] rowY = new float[20];
+	static float[] x = new float[20];
+	static float[] y = new float[20];
+	static float[] yP = new float[20];
+	static float[] wynik = new float[20];
+	static float[] wynikPoSumowaniu = new float[20];
+	private static float[] wspolczynnikWielomianu = new float[20];
 
 	public static void pobierzDane(float[] x, float[] y, float[] yP) {
 
 		Scanner in = new Scanner(System.in);
 		System.out.println("Witaj uzytkowniku!");
 		System.out.println("Podaja ilosc elementow");
-		// iloscElementowX = in.nextInt();
-		iloscElementowX = 3;
+		iloscElementowX = in.nextInt();
+		// iloscElementowX = 3;
 		// Wypelnianie tablicy x
 		for (int i = 0; i < iloscElementowX; i++) {
 
-			// System.out.println("Podaj wartosc x dla x" + i + ":");
-			// x[i] = in.nextInt();
-			x[0] = 0;
-			x[1] = 1;
-			x[2] = -1;
+			System.out.println("Podaj wartosc x dla x" + i + ":");
+			x[i] = in.nextInt();
+			// x[0] = 0;
+			// x[1] = 1;
+			// x[2] = 2;
 
+			// x[0] = 0;
+			// x[1] = 1;
+			// x[2] = -1;
 		}
 
 		// Wypelnianie tablicy y
 		for (int i = 0; i < iloscElementowX; i++) {
 
-			// System.out.println("Podaj wartosci y dla x" + i + ":");
-			// y[i] = in.nextInt();
-			y[0] = 2;
-			y[1] = 5;
-			y[2] = 7;
+			System.out.println("Podaj wartosci y dla x" + i + ":");
+			y[i] = in.nextInt();
+			// y[0] = 0;
+			// y[1] = 1;
+			// y[2] = 2;
+
+			// y[0] = 2;
+			// y[1] = 5;
+			// y[2] = 7;
 
 		}
 
@@ -53,14 +60,14 @@ public class Hermite {
 		System.out.println(parzystaIloscElementow);
 		for (int i = 0; i <= parzystaIloscElementow; i++) {
 
-			yP[0] = 1;
-			yP[1] = 0;
-			yP[2] = -1;
-			// if (!(parzystaIloscElementow <= i)) {
-			// System.out.println("Podaj wartosci yP dla x" + 2 * i + ":");
-			//
-			// //yP[2 * i] = in.nextInt();
-			// }
+			// yP[0] = 1;
+			// yP[1] = 0;
+			// yP[2] = -1;
+			if (!(parzystaIloscElementow <= i)) {
+				System.out.println("Podaj wartosci yP dla x" + 2 * i + ":");
+
+				yP[2 * i] = in.nextInt();
+			}
 		}
 		iloscElementow = iloscElementowX + parzystaIloscElementow;
 		in.close();
@@ -150,12 +157,10 @@ public class Hermite {
 				licznik++;
 				rowX[licznik] = x[i];
 				rowY[licznik] = y[i];
-
 				licznik++;
 			} else {
 				rowX[licznik] = x[i];
 				rowY[licznik] = y[i];
-
 				licznik++;
 			}
 		}
@@ -169,16 +174,11 @@ public class Hermite {
 	private static void zbudujWielomian(float[] wspolczynnikWielomianu,
 			float[] tabX) {
 		int licznikPrzebiegu = 1;
-		float[] Wx = new float[6];
+		float[] Wx = new float[20];
 		float[] Vx = new float[2];
 		wynikPoSumowaniu[0] = wspolczynnikWielomianu[0];
 
 		for (int i = 1; i < Wx.length - 1; i++) {
-			System.out.println("\n\nAktualne rozwiazanie");
-			for (float x : wynikPoSumowaniu) {
-				System.out.print(x + ", ");
-			}
-
 			int licznik = i;
 
 			if (licznikPrzebiegu == 1) {
@@ -190,31 +190,13 @@ public class Hermite {
 
 			}
 
-			// if (licznikPrzebiegu == 2) {
-			// int licznikX0 = 0;
-			// Wx[0] = wspolczynnikWielomianu[i];
-			// System.out
-			// .println("\nUstawiam wartosc "
-			// + wspolczynnikWielomianu[i] + "na indeksie: "
-			// + licznik);
-			// Vx[0] = 1;
-			// for (int k = 0; k < licznikPrzebiegu; k++) {
-			// Vx[1] = tabX[licznikX0];
-			// mnozenie(Wx, Vx);
-			// for (int j = 1; j < Wx.length; j++) {
-			// Wx[j] = wynik[j];
-			// }
-			// licznikX0++;
-			// Wx[0] = 0;
-			// }
-			// }
 			if (licznikPrzebiegu != 1) {
 				int licznikX0 = 0;
 				Wx[0] = wspolczynnikWielomianu[i];
-				System.out
-						.println("\nUstawiam wartosc "
-								+ wspolczynnikWielomianu[i] + "na indeksie: "
-								+ licznik);
+				// System.out
+				// .println("\nUstawiam wartosc "
+				// + wspolczynnikWielomianu[i] + "na indeksie: "
+				// + licznik);
 				Vx[0] = 1;
 
 				for (int k = 0; k < licznikPrzebiegu; k++) {
@@ -229,28 +211,6 @@ public class Hermite {
 
 			}
 
-			// if (licznikPrzebiegu == 4) {
-			// int licznikX0 = 0;
-			// Wx[0] = wspolczynnikWielomianu[i];
-			// System.out
-			// .println("\nUstawiam wartosc "
-			// + wspolczynnikWielomianu[i] + "na indeksie: "
-			// + licznik);
-			// Vx[0] = 1;
-			//
-			// for (int k = 0; k < 4; k++) {
-			// Vx[1] = tabX[licznikX0];
-			// mnozenie(Wx, Vx);
-			// for (int j = 0; j < Wx.length; j++) {
-			// Wx[j] = wynik[j];
-			// }
-			// licznikX0++;
-			// Wx[0] = 0;
-			// }
-			//
-			// }
-
-			System.out.println("\n WX po przypisaniu: ");
 			for (int j = 1; j < Wx.length; j++) {
 				wynikPoSumowaniu[j] += wynik[j];
 
@@ -262,22 +222,23 @@ public class Hermite {
 			}
 		}
 
-		System.out.println("\n \n Wynik");
+		System.out.println("\n \n WIELOMIAN");
 		for (float x : wynikPoSumowaniu) {
 			System.out.print(x + ", ");
 		}
 	}
 
 	private static void mnozenie(float[] Wx, float[] Vx) {
-		System.out.println("Elementy w tabeli Wx: ");
-		for (float x : Wx) {
-			System.out.print(x + ", ");
-		}
-
-		System.out.println("\n \nElementy w tabeli Vx: ");
-		for (float x : Vx) {
-			System.out.print(x + ", ");
-		}
+		// //Logi kontrolne
+		// System.out.println("Elementy w tabeli Wx: ");
+		// for (float x : Wx) {
+		// System.out.print(x + ", ");
+		// }
+		//
+		// System.out.println("\n \nElementy w tabeli Vx: ");
+		// for (float x : Vx) {
+		// System.out.print(x + ", ");
+		// }
 
 		wynik[0] = (-1) * Wx[0] * Vx[1];
 		for (int i = 1; i <= iloscElementowX + 2; i++) {
@@ -285,17 +246,17 @@ public class Hermite {
 
 			if (i == iloscElementowX + 1) { // maksymalny wspolczynnik
 				wynik[i] = Wx[--licznik];
-				System.out.println(wynik[i]);
+				// System.out.println(wynik[i]);
 			} else {
-				System.out.println();
+				// System.out.println();
 				wynik[i] = Wx[--licznik] - Wx[i] * Vx[1];
-				System.out.println(wynik[i]);
+				// System.out.println(wynik[i]);
 			}
 		}
-		System.out.println("\n \nElementy w tabeli Wynik: ");
-		for (float x : wynik) {
-			System.out.print(x + ", ");
-		}
+		// System.out.println("\n \nElementy w tabeli Wynik: ");
+		// for (float x : wynik) {
+		// System.out.print(x + ", ");
+		// }
 
 	}
 
